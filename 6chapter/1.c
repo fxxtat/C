b45e6d8036bad329cdfc9bd919287dc228126d88
fxxtat <fxxtat@gmail.com>
2016-12-09 23:06:47 +0800

    first

--- 6chapter/1.c
+++ 6chapter/1.c
@@ -1,7 +1,15 @@
-/*创建一个包含2个元素的数组，并在其中储存26个小写字母。然后打印数组的所有内容*/
 #include<stdio.h>
+#define SIZE 26
+/*创建一个包含26个元素的数组，并在其中储存26个小写字母。然后打印数组的所有内容*/
 int main()
 {
-
+    int i;
+    char lcase[26];
+    for(i = 0;i < SIZE;i++)  
+        lcase[i] = 'a' + i;
+    for (i = 0;i < SIZE;i++)
+        printf("%c",lcase[i]);
+    printf("\n");
+    getchar();U
     return 0;
 }
