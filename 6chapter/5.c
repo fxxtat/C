cdb19a81fe85740d6de2747d19576ef5074a669c
fxxtat <fxxtat@gmail.com>
2016-12-10 23:22:50 +0800

    first

--- 6chapter/5.c
+++ 6chapter/5.c
@@ -5,3 +5,21 @@
  ABCDCBA
 ABCDEDCBA
 */
+#include<stdio.h>
+int main()
+{
+    for (int i=1;i<6;i++)
+    {
+        for (int j=5;j>i;j--)
+        {
+            printf(" ");
+        }
+        for (int k=0;k<i;k++)
+        {
+            printf("%c",'A'+k);
+        }
+        printf("\n");
+    }
+    getchar();
+    return 0;
+}
