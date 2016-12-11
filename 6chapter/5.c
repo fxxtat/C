22153e55859ced2f09ffd1beb7dbd3cc12d66d4e
fxxtat <fxxtat@gmail.com>
2016-12-11 21:37:06 +0800

    first

--- 6chapter/5.c
+++ 6chapter/5.c
@@ -5,18 +5,29 @@
  ABCDCBA
 ABCDEDCBA
 */
+//法一
+
 #include<stdio.h>
 int main()
 {
-    for (int i=1;i<6;i++)
+    int sp;
+    char c;
+    for (int i=1;i<6;i++)//输出6行
     {
-        for (int j=5;j>i;j--)
+        for (int j=5;j>i;j--)//输出空格
         {
             printf(" ");
         }
-        for (int k=0;k<i;k++)
+        for (c='A';c<65+i;c++)//输出A-当行最大的字母65+i（当前行数）
         {
-            printf("%c",'A'+k);
+            printf("%c",c);
+        }
+        for (c;c-1>65;c--)
+        /*此时经过上面一个for循环，c已是当前行最大的一个字母。
+        开始输出余下的字母,输出到A
+        */
+        {
+            printf("%c",c-2);
         }
         printf("\n");
     }
--- 6chapter/6.c
+++ 6chapter/6.c
@@ -1,3 +1,20 @@
 /*
-编写一个程序打印一个表格，每一行打印一个整数，该数的平方，该数的立方。要求用户输入表格的上下限。是哟哦那个一个for循环
+编写一个程序打印一个表格，每一行打印一个整数，该数的平方，该数的立方。要求用户输入表格的上下限。使用一个for循环
 */
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+
+    printf("input one number");
+    scanf("%d",&n1);
+    printf("input another number");
+    scanf("%d",&n2);
+
+    for (n1;n1<=n2;n1++)
+    {
+        printf("%d  %d  %d",n1,n1*n1,n1*n1*n1);
+    }
+    getchar();
+    return 0;
+}
